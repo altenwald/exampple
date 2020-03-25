e1611dd4d26d20e48f8820c6367a0918bd0c443b
Manuel Rubio <manuel@altenwald.com>
2020-03-25 14:21:49 +0100

    add test for stanza generator

---
+++ test/xmpp/stanza_test.exs
@@ -0,0 +1,31 @@
+defmodule Exampple.Xmpp.StanzaTest do
+  use ExUnit.Case
+
+  defmodule Person do
+    use Exampple.Xmpp.Stanza
+    alias Exampple.Xml.Xmlel
+
+    @impl Exampple.Xmpp.Stanza
+    def render(person) do
+      Xmlel.new("person", %{}, [
+        Xmlel.new("name", %{}, [to_string(person.name)]),
+        Xmlel.new("age", %{"years-old" => to_string(person.age)}, [])
+      ])
+    end
+
+    defstruct name: nil, age: nil
+  end
+
+  describe "stanza tests" do
+    test "check serialization" do
+      alice = %Exampple.Xmpp.StanzaTest.Person{name: "Alice", age: 25}
+      bob = %Exampple.Xmpp.StanzaTest.Person{name: "Bob", age: 21}
+
+      assert "<person><name>Alice</name><age years-old=\"25\"/></person>" ==
+               Saxy.encode!(Saxy.Builder.build(alice), nil)
+
+      assert "<person><name>Bob</name><age years-old=\"21\"/></person>" ==
+               Saxy.encode!(Saxy.Builder.build(bob), nil)
+    end
+  end
+end
