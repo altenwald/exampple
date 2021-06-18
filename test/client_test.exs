3a38fb48bdb27b77d50cb107be22cb0d0375e756
Manuel Rubio <manuel@altenwald.com>
2021-06-18 02:56:09 +0200

    add missing test changes

--- test/client_test.exs
+++ test/client_test.exs
@@ -2,6 +2,7 @@ defmodule Exampple.ClientTest do
   use ExUnit.Case, async: false
 
   import Exampple.Xml.Xmlel
+  import Exampple.Router.ConnCase.Client
 
   alias Exampple.{Client, DummyTcpClient}
   alias Exampple.Router.Conn
@@ -9,7 +10,7 @@ defmodule Exampple.ClientTest do
 
   describe "connectivity" do
     test "starting" do
-      assert {:ok, pid} = Client.start_link(%{
+      assert {:ok, _pid} = Client.start_link(%{
         host: "example.com",
         port: 5222,
         domain: "example.com",
